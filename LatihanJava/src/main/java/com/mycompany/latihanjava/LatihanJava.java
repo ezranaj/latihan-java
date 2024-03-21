5b64d93f840d6848f9bb61e364829a80d3b5053a
ezranaj <ezranaj03@gmail.com>
2024-03-21 16:38:45 +0700

    latihan 1

---
+++ LatihanJava/src/main/java/com/mycompany/latihanjava/LatihanJava.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.latihanjava;
+
+/**
+ *
+ * @author ezran
+ */
+public class LatihanJava {
+
+    public static void main(String[] args) {
+        int jari;
+        double keliling;
+        final double phi = 3.14;
+        
+        jari = 70;
+        
+        keliling = 2*phi*jari;
+        
+        System.out.println("Keliling = "+keliling);
+    }
+}
